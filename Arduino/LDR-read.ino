005d371244a853ca1322660063ced5d8b3e424fe
rohamid <rohamid90@gmail.com>
2024-05-09 19:52:02 +0700

    Added low pass filter

--- Arduino/LDR-read.ino
+++ Arduino/LDR-read.ino
@@ -17,10 +17,24 @@
 //			    |		    ---------
 // ---------
 
-int nilai = 0;
+/* Struktur low pass filter */
+typedef struct lpf_config_t {
+	float value;
+	float valueNew;
+	float valueOld;
+	float factor;
+}lpf_config_t;
+
+lpf_config_t filterLDR;
+
+
+int nilai = 0;	// variable ini menyimpan nilai sebelum low-pass-filter
+int nilaiTerfilter = 0;	// variable ini menyimpan nilai setelah low-pass-filter
 
 void setup() {
 	Serial.begin(9600);
+	// Lakukan inisialisasi filter.
+	lpf_init(&filterLDR, .02);
 }
 
 void loop() {
@@ -34,8 +48,40 @@ void loop() {
 
 	// Hitung rerata dari pembacaan 5 LDR
 	nilai = nilai / 5;
-	Serial.print("Nilai Sensor: ");
-	Serial.print(nilai);
-	Serial.print("\n");
-	delay(500);
+	nilaiTerfilter = (int)lpf_get_filter(&filterLDR, nilai);
+  	Serial.print("Sensor:");
+  	Serial.print(nilai);
+  	Serial.print(",");
+  	Serial.print("Filter:");
+  	Serial.print(nilaiTerfilter);
+  	Serial.print("\n");
+
+  	delay(100);
+}
+
+/* @fn: lpf_init
+ * @brief: Inisialisasi low pass filter. 
+ * @param: lpf 				: pointer ke struct lpf_config_t
+ * @param: filter_factor 	: faktor filterisasi (sesuaikan perubahan nilai setelah difilter.) 
+ * @return: None
+ * @note:
+ */
+void lpf_init(lpf_config_t *lpf, float filter_factor) {
+	lpf->value = lpf->valueNew = lpf->valueOld = 0.0;
+	lpf->factor = filter_factor;
+}
+
+/* @fn: lpf_get_filter
+ * @brief: Filter sinyal input dan dapatkan sinyal terfilter dari return. 
+ * @param: lpf 		: pointer ke struct lpf_config_t
+ * @param: input 	: sinyal yang akan di filter
+ * @return: None
+ * @note:
+ */
+float lpf_get_filter(lpf_config_t *lpf, float input) {
+	lpf->value = input;
+	lpf->valueNew = ((1.0 - lpf->factor) * lpf->valueOld) + (lpf->factor * lpf->value);
+	lpf->valueOld = lpf->valueNew;
+
+	return (lpf->valueNew);
 }
